df01591bff0a01130febcf52aa5f4c7b165cdb08
54 Bridge <54bridgeapp@gmail.com>
2020-01-05 14:44:52 -0600

    refractor date and added todo list functionality

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const whichDay = require("./helper/helper");
+
+let items = [];
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   const today = new Date();
-  const currentDay = today.getDay();
 
-  const findDay = whichDay(currentDay);
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
 
-  res.render("list", { kindOfDay: findDay });
+  const day = today.toLocaleDateString("en-us", options);
+
+  res.render("list", { kindOfDay: day, newTodos: items });
+});
+
+app.post("/", (req, res) => {
+  if (!req.body) return res.json("Can not be empty");
+
+  const item = req.body.newTodo;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 const Port = process.env.PORT || 3000;
