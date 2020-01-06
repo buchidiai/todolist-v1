644f4b4cfa53bd731f48dd625c699c7c48191344
54 Bridge <54bridgeapp@gmail.com>
2020-01-05 18:55:36 -0600

    added header n footer & proper routing

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 let items = [];
+let workitems = [];
 
 const app = express();
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.set("view engine", "ejs");
 
@@ -23,18 +26,36 @@ app.get("/", (req, res) => {
 
   const day = today.toLocaleDateString("en-us", options);
 
-  res.render("list", { kindOfDay: day, newTodos: items });
+  res.render("list", {
+    listTitle: day,
+    newTodos: items
+  });
 });
 
 app.post("/", (req, res) => {
   if (!req.body) return res.json("Can not be empty");
 
+  console.log(req.body);
+
   const item = req.body.newTodo;
 
-  items.push(item);
+  if (req.body.list === "Work") {
+    workitems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
 
-  res.redirect("/");
+    res.redirect("/");
+  }
 });
+
+app.get("/work", (req, res) => {
+  res.render("list", {
+    listTitle: "Work List",
+    newTodos: workitems
+  });
+});
+
 
 const Port = process.env.PORT || 3000;
 app.listen(Port, () => {
