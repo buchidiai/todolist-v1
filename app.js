6f8086e427a8043edf90cf0e1fb0a9efe63c3496
54 Bridge <54bridgeapp@gmail.com>
2020-01-07 18:11:24 -0600

    fixed render of items

--- app.js
+++ app.js
@@ -45,8 +45,11 @@ const defaultItems = [
 ];
 
 app.get("/", (req, res) => {
+  //get all items from db
   Item.find({}).then(response => {
     if (response.length === 0) {
+      //if array length is 0, insert default into db
+      //then redirect
       Item.insertMany(defaultItems)
         .then(() => {
           console.log(`saved default to db`);
@@ -54,6 +57,8 @@ app.get("/", (req, res) => {
         })
         .catch(() => console.log("error saving default"));
     } else {
+      //if rayy is not 0
+      //render items
       res.render("list", {
         listTitle: "Today",
         newTodos: response
@@ -64,11 +69,11 @@ app.get("/", (req, res) => {
 
 app.get("/:list", (req, res) => {
   const listName = req.params.list;
-
-  List.find({ name: listName }).then(result => {
-    if (!result) {
+  //find list based on params entered
+  List.findOne({ name: listName }).then(result => {
+    if (result === null) {
       //create new list
-      console.log(result, "result not");
+      //list doesnt exisit
 
       const list = new List({
         name: listName,
@@ -78,9 +83,6 @@ app.get("/:list", (req, res) => {
         .save()
         .then(r => {
           console.log("saved " + listName);
-
-          console.log("response from new list " + r);
-
           res.redirect("/" + listName);
         })
         .catch(err => {
@@ -88,38 +90,32 @@ app.get("/:list", (req, res) => {
         });
     } else {
       //show existing list
-      console.log(result, "true");
+      //show array items
 
       res.render("list", {
-        listTitle: listName,
-        newTodos: result
+        listTitle: result.name,
+        newTodos: result.items
       });
     }
   });
 });
 
 app.post("/", (req, res) => {
+  //check if body is empty
   if (!req.body || !req.body.newTodo || !req.body.list)
     return res.json("Can not be empty");
 
   const item = req.body.newTodo;
+  const listName = req.body.list;
+  //create new item object
+  const todo = new Item({
+    name: item
+  });
 
-  if (req.body.list === "Work") {
-    const work = new Work({
-      name: item
-    });
+  console.log(listName, "listName");
 
-    work
-      .save()
-      .then(() => {
-        console.log("saved " + item);
-        res.redirect("/work");
-      })
-      .catch(() => console.log("error saving work item"));
-  } else {
-    const todo = new Item({
-      name: item
-    });
+  if (listName === "Today") {
+    //save item from main page to db
 
     todo
       .save()
@@ -129,11 +125,40 @@ app.post("/", (req, res) => {
       .catch(() => console.log("error saving todo items "));
 
     res.redirect("/");
+  } else {
+    //when item is added from custom list w/ param handle
+    //find object from db
+    //push new item into array of items from listSchema
+    //save to db
+
+    List.findOne({ name: listName })
+      .then(result => {
+        console.log(result, "result from custom param");
+
+        console.log("push");
+        result.items.push(todo);
+        result.save();
+        res.redirect("/" + listName);
+
+        // if (!result) {
+        //   //create new list
+        //   console.log("create");
+        // } else {
+        //   //push to list
+
+        // }
+      })
+      .catch(err => {
+        console.log("error" + err);
+      });
   }
 });
 
 app.post("/delete", (req, res) => {
   const checkedItem = req.body.checkbox;
+
+  //remove item from db via id
+  //redirect
 
   Item.findByIdAndRemove(checkedItem)
     .then(result => {
